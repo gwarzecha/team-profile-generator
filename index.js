0a11d76c5c1da4a3cb7eb02bebc1d21711b37106
gwarz <gmwarzecha@tutanota.com>
2021-02-20 15:51:41 -0700

    created prompts for role and follow up info questions

---
+++ index.js
@@ -0,0 +1,78 @@
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const Manager = require('./lib/Manager');
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+
+const employees = [];
+
+function newEmployee() {
+  inquirer.prompt([
+    {
+      type: 'list',
+      name: 'role',
+      message: 'Choose a role:',
+      choices: ['Engineer', 'Intern', 'Manager',],
+    },
+    {
+      type: 'input',
+      name: 'name',
+      message: 'Enter employee name:',
+    },
+    {
+      type: 'input',
+      name: 'id',
+      message: 'Enter employee ID:',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'Enter employee email address:'
+    }
+  ])
+    .then((responses) => {
+      if (responses.role === 'Engineer') {
+        inquirer.prompt([
+          {
+            type: 'input',
+            name: 'github',
+            message: 'Enter employee GitHub username:'
+          }
+        ])
+          .then((additional) => {
+            const engineer = new Engineer(responses.name, responses.id, responses.email, additional.github);
+            employees.push(engineer);
+            console.log(employees);
+          })
+      } else if (responses.role === 'Intern') {
+        inquirer.prompt([
+          {
+            type: 'input',
+            name: 'school',
+            message: 'Enter employee school:'
+          }
+        ])
+        .then((additional) => {
+          const intern = new Intern(responses.name, responses.id, responses.email, additional.school);
+          employees.push(intern);
+          console.log(employees);
+        })
+      } else if (responses.role === 'Manager') {
+        inquirer.prompt([
+          {
+            type: 'input',
+            name: 'office',
+            message: 'Enter employee office number:'
+          }
+        ])
+        .then((additional) => {
+          const manager = new Manager(responses.name, responses.id, responses.email, additional.office);
+          employees.push(manager);
+          console.log(employees);
+        })
+      }
+    })
+}
+
+newEmployee();
